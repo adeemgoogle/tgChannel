950ca33bf2ed65a703a4aeffc02d997bdf54507f
Adeem <75906616+adeemgoogle@users.noreply.github.com>
2025-05-10 19:45:07 +0500

    Revert "tetst"
    This reverts commit ab103469a04f0f53cf0cd32006bbfdd42b3c0e3d.

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	fmt.Println("\nТестируем машину с турбонаддувом:")
 	patterns.TestDrive(turboCar)
 
-	fmt.Println("\nТестируем машину с турбонаддувом и спортивным выхлопом jjjjj111j:")
+	fmt.Println("\nТестируем машину с турбонаддувом и спортивным выхлопом jjjjjj:")
 	patterns.TestDrive(sportsCar)
 
 	// adapter
